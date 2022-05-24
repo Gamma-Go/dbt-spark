3b8f3ed055b98354f97e1d1db7095545983d4e08
GammaGo <shijiake@deepexi.com>
2022-05-24 09:54:26 +0800

    dwd

---
+++ models/dwd/dwd_order_detail_v2.sql
@@ -0,0 +1,29 @@
+with orders as (
+    select * from {{ref('stg_order')}}
+),
+
+memeber as (
+    select * from {{ref('stg_member')}}
+),
+
+payment as (
+    select * from {{ref('stg_payment')}}
+),
+
+dwd_order_detail as (
+    select
+        o.order_id,
+        o.member_id,
+        o.gmt_created,
+        m.name,
+        m.address,
+        p.payment_id,
+        o.real_amount,
+        p.amount as payment_amount,
+        p.gmt_created as payment_gmt_created
+    from orders o
+    left join member m on o.member_id = m.member_id
+    left join payment p on o.order_id = p.order_id
+)
+
+select * from dwd_order_detail
